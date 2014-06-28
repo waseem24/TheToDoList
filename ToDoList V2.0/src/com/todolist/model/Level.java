f02feea6fa65a6349bac0d4f00868b7509404eae
Siyanda <cece.marela@gmail.com>
2014-06-28 16:38:14 +0200

    commiting the Level Pojo

---
+++ ToDoList V2.0/src/com/todolist/model/Level.java
@@ -0,0 +1,69 @@
+package com.todolist.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.NotBlank;
+import org.hibernate.validator.constraints.NotEmpty;
+
+@Entity
+@Table(name="tblLevel")
+public class Level implements Serializable{
+
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name="tblLevel_Id", unique=true)
+	private Long levelId;
+	
+	@NotBlank
+	@NotEmpty
+	@NotNull
+	@Column(name="tblPercentage")
+	private double percentage;
+	
+	@NotBlank
+	@NotEmpty
+	@NotNull
+	@Column(name="tblDiscription")
+	private String discription;
+
+	public Level() {
+	}
+
+	public Long getLevelId() {
+		return levelId;
+	}
+
+	public void setLevelId(Long levelId) {
+		this.levelId = levelId;
+	}
+
+	public double getPercentage() {
+		return percentage;
+	}
+
+	public void setPercentage(double percentage) {
+		this.percentage = percentage;
+	}
+
+	public String getDiscription() {
+		return discription;
+	}
+
+	public void setDiscription(String discription) {
+		this.discription = discription;
+	}
+	
+	
+
+}
